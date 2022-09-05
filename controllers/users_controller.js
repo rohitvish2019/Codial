81c055f36a5411560680c0f4a1f10eaf77e8456b
Rohit Vishwakarma <rohitbca2019@gmail.com>
2022-09-05 09:47:59 +0530

    Added sessions and displayed some dynamic data on views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,6 +10,9 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -18,6 +21,9 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
@@ -49,4 +55,17 @@ module.exports.create = function(req, res){
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
     return res.redirect('/');
+}
+
+
+module.exports.destroySession = function(request, response){
+    request.logout( function(err){
+        if(err){
+            console.log("Error logging out");
+        }
+        else{
+            console.log("Logged out successfully");
+        }
+    });
+    return response.redirect('/users/sign-in')
 }
